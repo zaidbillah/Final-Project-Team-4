b84b8a07764118c86895f4449f7853b5b7442897
gazalisaleh <gazalisaleh78@gmail.com>
2023-01-20 21:30:42 +0800

    automation script

---
+++ gazali - automation testing/TL01-selenium-gazali.py
@@ -0,0 +1,17 @@
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+
+driver = webdriver.Chrome()
+driver.get("https://itera-qa.azurewebsites.net/Login")
+
+driver.find_element(By.ID,"Username").send_keys("admin")
+driver.find_element(By.ID, "Password").send_keys("admin")
+
+#driver.find_element_by_class_name("btn btn-primary").click()
+driver.find_element(By.CSS_SELECTOR, ".btn.btn-primary").click()
+
+# Cek kondisi login
+if "https://itera-qa.azurewebsites.net/Dashboard" in driver.current_url:
+    print("Login berhasil, Anda sekarang berada di halaman dashboard")
+else:
+    print("Login gagal, pastikan username dan password yang Anda masukkan benar")
---
+++ gazali - automation testing/TL03-selenium-gazali.py
@@ -0,0 +1,17 @@
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+
+driver = webdriver.Chrome()
+driver.get("https://itera-qa.azurewebsites.net/Login")
+
+driver.find_element(By.ID,"Username").send_keys("")
+driver.find_element(By.ID, "Password").send_keys("")
+
+#driver.find_element_by_class_name("btn btn-primary").click()
+driver.find_element(By.CSS_SELECTOR, ".btn.btn-primary").click()
+
+# Cek kondisi login
+if "alert-danger" in driver.page_source:
+    print("passed")
+else:
+    print("failed")
